47c14c09fe51ce977c37ddad6d588098f5574342
向兴 <xiangxing@ave40.com>
2018-09-20 18:38:55 +0800

    改版重构

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+    var homeBannerSwiper = new Swiper ('.home-banner', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.home-banner .swiper-pagination',
+        }
+    });
+
+    var suitSwiper = new Swiper ('.suit-swiper', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.suit-swiper .swiper-pagination',
+        }
+    })
+
+});
