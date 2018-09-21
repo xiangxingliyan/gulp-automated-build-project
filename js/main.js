8b4bb9ee9b39575327508e5cc3df7dc385d48aba
向兴 <xiangxing@ave40.com>
2018-09-21 17:36:05 +0800

    vladdin国内站开发完成

--- js/main.js
+++ js/main.js
@@ -1,18 +1,69 @@
 $(function () {
+
     var homeBannerSwiper = new Swiper ('.home-banner', {
-        // Optional parameters
+
+        autoplay: {
+            delay: 4000,
+            stopOnLastSlide: false,
+            disableOnInteraction: false,
+        },
         loop: true,
+        speed: 1000,
         pagination: {
             el: '.home-banner .swiper-pagination',
+            clickable: true
         }
     });
 
     var suitSwiper = new Swiper ('.suit-swiper', {
-        // Optional parameters
+        autoplay: true,
         loop: true,
+        speed: 1000,
+        delay: 4000,
         pagination: {
             el: '.suit-swiper .swiper-pagination',
+            clickable: true
+        },
+        on: {
+            slideChange: function(){
+                controlRightIconSlider(this.realIndex);
+            }
         }
-    })
+    });
+
+    /* 控制右侧icon切换 */
+    function controlRightIconSlider(index) {
+        var $colorItem = $('.suit-info .color-list li');
+        var $textItem = $('.suit-text').find('li');
+
+        $colorItem.eq(index).addClass('active').siblings('li').removeClass('active');
+        $textItem.eq(index).addClass('on').removeClass('hide').siblings('li').removeClass('on').addClass('hide');
+        setTimeout(function () {
+            $textItem.removeClass('hide');
+        },1000);
+    }
+
+    $('.suit-info .color-list li').click(function () {
+        var index = $(this).index();
+
+        controlRightIconSlider(index);
+        suitSwiper.slideToLoop(index);
+    });
+
+    /* jq实现锚点动画效果 */
+    $('a[href*=#],area[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top - 90;
+                $('html,body').animate({
+                        scrollTop: targetOffset
+                    },
+                    1000);
+                return false;
+            }
+        }
+    });
 
 });
